c34c00f0459ab117037235fd18f0d53b8bb1212a
Manapon Sanguanklin <manapon7330@gmail.com>
2023-08-09 13:21:29 +0700

    add bigBark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ManaponS/puppy
 
 go 1.20
+
+require github.com/ManaponS/dog v0.0.0-20230809061832-07ee8b10fda5 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/ManaponS/dog"
+)
+
 func Bark() string {
 	return "Woof"
 }
 
 func Barks() string {
 	return "Woof wOOf wooF"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
